d829044bfb84f53018e3c954ec055e8651cf0c35
Stacey Sharp <ssharpjr@gmail.com>
2021-10-20 09:13:26 -0400

    Added OWT

--- edi_inbound_rename.py
+++ edi_inbound_rename.py
@@ -27,6 +27,7 @@ ga_spartanburg_isa = "US080950568SPA"
 ga_tennessee_isa = "GA808659114"
 husqvarna_isa = "HUSQORNGBRG"
 navistar_isa = "781495650"
+owt_isa = "827942173"
 
 
 # File Paths are for Windows OS
@@ -194,6 +195,12 @@ def rename_file_husq(filename):
 # Autoneum Begin
 ###############################################################################
 def rename_file_autoneum(filename):
+    # Ship From: 140472
+    # Ship To Locations
+    # THM Only: US03
+    # HOW Only: MX02, US02
+    # Both THM and HOW use US01
+
     f = filename  # Raw file name
     f_path = os.path.join(staging_dir, filename)  # file name with path
 
@@ -261,6 +268,41 @@ def rename_file_navistar(filename):
 # Navistar End
 ###############################################################################
 
+
+###############################################################################
+# OWT/TTI/Ryobi Begin
+###############################################################################
+def rename_file_owt(filename):
+    f = filename  # Raw file name
+    f_path = os.path.join(staging_dir, filename)  # file name with path
+
+    # Check if in ECGrid format.
+    # ECGrid file format: 1027-20201006101520-2e7441af.edi
+    if not f.startswith("1027"):
+        # Not an ECGrid file
+        return
+
+    sep = "-" # File separator
+    f_ext = ".edi"  # File extension
+    f = os.path.splitext(f)[0]  # Strip extension
+    f_list = f.split(sep)  # Make a list from the split    
+    f_date = f_list[1]  # The second piece is the date code
+    f_idx = f_list[2]  # The third piece is the index
+    f_type = get_file_type(f_path)
+
+
+    isa = get_isa(f_path)
+    if isa != owt_isa:
+        return
+
+    new_filename = "OWT" + sep + f_type + sep + f_date + sep + f_idx + f_ext
+    old_filename = os.path.join(staging_dir, filename)
+    new_filename = os.path.join(in_dir, new_filename)
+    os.rename(old_filename, new_filename)
+    print(old_filename + '  >  ' + new_filename)
+###############################################################################
+# OWT/TTI/Ryobi End
+###############################################################################
 
 if __name__ == '__main__':
     process_staging_dir()
